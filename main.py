e29cbd2afc9993c4c86ceb3243e09d9b6584e24c
Kamalesh-Dev-Web <kamalsurea619@gmail.com>
2022-11-26 08:39:52 +0530

    make it better

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from fastapi import FastAPI
 
 from movies import movies_router
 from database import init_db
+from mangum import Mangum
 
 app = FastAPI()
+handler=Mangum(app)
 
 app.include_router(movies_router,prefix='/movies')
 
